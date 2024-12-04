85075e393a0b4fb7f239779732890f82ffd97fee
Rocio <deberesrocio@gmail.com>
2024-12-04 11:40:42 +0000

    home terminado

--- myProyect/src/Home.jsx
+++ myProyect/src/Home.jsx
@@ -1,8 +1,8 @@
-import {useState} from 'react';
+import { useState } from "react";
 import "./Home.css";
 
 const Home = () => {
-const [showLogin, setShowLogin] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
   const specialtiesOBJ = [
     {
@@ -42,7 +42,7 @@ const [showLogin, setShowLogin] = useState(false);
         </>
       );
     });
-  }
+  };
   return (
     <div className="home-container">
       <header className="header">
@@ -58,8 +58,12 @@ const [showLogin, setShowLogin] = useState(false);
             <p>Log in as a doctor or patient</p>
             <div className="banner-buttons">
               <button className="register-banner-button">Register</button>
-              <button className="access-banner-button" onClick={() => setShowLogin(true)}>
-              Log in</button>
+              <button
+                className="access-banner-button"
+                onClick={() => setShowLogin(true)}
+              >
+                Log in
+              </button>
             </div>
           </div>
         </div>
@@ -71,39 +75,47 @@ const [showLogin, setShowLogin] = useState(false);
       </main>
 
       <footer className="footer">
-        <div className="footer-links">
-          <a href="/">Home</a>
-          <a href="/">Specialities</a>
-          <a href="/">Contact</a>
-        </div> 
         <div>
-          <img className="logotipo" src="../public/logotipo.png"></img>
-          <p className='logo-test'>This is our website to access medical services in an easy and simple way, you can consult and request your changes.</p>
+          <img className="logotipo-f" src="../public/logotipo.png"></img>
+          <p className="logo-test">
+            This is our website to access medical services in an easy and simple
+            way, you can consult and request your changes.
+          </p>
         </div>
-        <div className='footer-img'>
-          <img src='../public/instagram.png' />
-          <img src='../public/x.png' />
-          <img src='../public/facebook.png' />
-        </div>
-        
-        <div className="footer-info">
-          <img src="../public/llamar.png" />
-          <p>998-725-255</p>
-        </div>
-        <div className="footer-info">
-          <img src="../public/localizacion.png" />
-          <p>Luis Morote 8, Las Palmas de Gran Canaria</p>
-        </div>
-        <div className="footer-info">
-          <img src="../public/correo.png" />
-          <p>medicare@hospital.com</p>
+        <div className="footer-content">
+          <div className="websites">
+            <div className="footer-links">
+              <a href="/">Home</a>
+              <a href="/">Specialities</a>
+              <a href="/">Contact</a>
+            </div>
+            <div className="footer-img">
+              <img src="../public/instagram.png" />
+              <img src="../public/x.png" />
+              <img src="../public/facebook.png" />
+            </div>
+          </div>
+          <div className="footer-info-parent">
+            <div className="footer-info">
+              <img src="../public/llamar.png" />
+              <p>998-725-255</p>
+            </div>
+            <div className="footer-info">
+              <img src="../public/localizacion.png" />
+              <p>Luis Morote 8, Las Palmas de Gran Canaria</p>
+            </div>
+            <div className="footer-info">
+              <img src="../public/correo.png" />
+              <p>medicare@hospital.com</p>
+            </div>
+          </div>
+          <div className="final">
+            <hr />
+            <p>© 2024 MediCare | All Rights Reserved</p>
+          </div>
         </div>
       </footer>
-      <div className="final">
-        <hr />
-        <p>© 2024 MediCare | All Rights Reserved</p>
-      </div>
-    
+
       {showLogin && (
         <div className="modal-overlay">
           <div className="modal-content">
@@ -120,21 +132,27 @@ const [showLogin, setShowLogin] = useState(false);
               </button>
             </form>
             <div className="links">
-              <a href="https://accounts.google.com/v3/signin/recoveryidentifier?ard=AHwGkRlCdvbl0AEXUjNnXxa8ho21NVUrW6V8vV3werBju8Vj4AKOJonOfCS09rLSszKeF-MCowRcQuxdgSGG5PBwFz1Ppd7WENTvCrUeCa2vHzK0Ht3p6FM&fpOnly=1&hl=en&service=mail&flowName=GlifWebSignIn&dsh=S-468234728%3A1733264926697843&ddm=1" target="_blank" rel="noopener noreferrer">Forgot your password?</a>
+              <a
+                href="https://accounts.google.com/v3/signin/recoveryidentifier?ard=AHwGkRlCdvbl0AEXUjNnXxa8ho21NVUrW6V8vV3werBju8Vj4AKOJonOfCS09rLSszKeF-MCowRcQuxdgSGG5PBwFz1Ppd7WENTvCrUeCa2vHzK0Ht3p6FM&fpOnly=1&hl=en&service=mail&flowName=GlifWebSignIn&dsh=S-468234728%3A1733264926697843&ddm=1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Forgot your password?
+              </a>
               <a href="#register">Don't have an account? Register</a>
             </div>
             <button
-              className="close-button" onClick={() => setShowLogin(false)}
-            > Close </button>
+              className="close-button"
+              onClick={() => setShowLogin(false)}
+            >
+              {" "}
+              Close{" "}
+            </button>
           </div>
         </div>
       )}
-
     </div>
   );
 };
 
-
-
 export default Home;
-
